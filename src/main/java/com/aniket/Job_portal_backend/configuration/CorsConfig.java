18d5d09663a3be319f55716163f035f340cc2a9a
Aniket code web <aniketmuni923@gmail.com>
2026-02-28 00:53:17 +0530

    added frontend url in corsconfig

--- src/main/java/com/aniket/Job_portal_backend/configuration/CorsConfig.java
+++ src/main/java/com/aniket/Job_portal_backend/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://job-portal-frontend-gv5gqgrch-aniket-munis-projects.vercel.app")
+                        .allowedOrigins("https://job-portal-frontend-gv5gqgrch-aniket-munis-projects.vercel.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
